0e3432ea57987c0619c02bc54259420ec3c39f54
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-02-15 18:58:14 +0530

    first commit & Login

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
