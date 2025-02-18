28f639e203b298ef0871a154a9b8910ad0746765
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-02-18 21:55:36 +0530

    script bug fix

--- backend/database.ts
+++ backend/database.ts
@@ -27,8 +27,7 @@ export const connectDb = async () => {
 
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
+        username VARCHAR(255) NOT NULL,
         password TEXT NOT NULL
       );
     `);
